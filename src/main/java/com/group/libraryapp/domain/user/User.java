52acdb0c596e8ee7779a00a84f1849a0975c99d4
Leehyoju97 <dlgywn2420@gmail.com>
2023-12-30 03:30:01 +0900

    refactor: 유저 리펙토링

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -1,8 +1,18 @@
 package com.group.libraryapp.domain.user;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 20)
     private String name;
     private Integer age;
+
+    protected User(){}
 
     public User(String name, Integer age) {
         if (name == null || name.isBlank()) {
@@ -12,11 +22,19 @@ public class User {
         this.age = age;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public String getName() {
         return name;
     }
 
     public Integer getAge() {
         return age;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
     }
 }
