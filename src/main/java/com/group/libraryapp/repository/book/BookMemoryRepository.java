8692f9923e5cb196ff2431550f421b553f0b93fd
Leehyoju97 <dlgywn2420@gmail.com>
2023-12-30 03:31:02 +0900

    feat: 책 등록

---
+++ src/main/java/com/group/libraryapp/repository/book/BookMemoryRepository.java
@@ -0,0 +1,12 @@
+package com.group.libraryapp.repository.book;
+
+import java.util.ArrayList;
+
+public class BookMemoryRepository {
+
+//    private final List<Book> books = new ArrayList<>();
+
+    public void saveBook() {
+//        books.add(new Book())
+    }
+}
